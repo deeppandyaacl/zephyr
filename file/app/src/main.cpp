e8e7aba37dd3bdc48477ef28f757d364c8ec78eb
deeppandya <deep.pandya@acldigital.com>
2025-02-06 10:11:37 +0530

    Updated yml file 1

--- file/app/src/main.cpp
+++ file/app/src/main.cpp
@@ -1,30 +1,3 @@
-/**
-* SIBEL INC ("SIBEL HEALTH") CONFIDENTIAL
-*
-* Copyright 2018-2024 [Sibel Inc.], All Rights Reserved.
-*
-* NOTICE: All information contained herein is, and remains the property of SIBEL
-* INC. The intellectual and technical concepts contained herein are proprietary
-* to SIBEL INC and may be covered by U.S. and Foreign Patents, patents in
-* process, and are protected by trade secret or copyright law. Dissemination of
-* this information or reproduction of this material is strictly forbidden unless
-* prior written permission is obtained from SIBEL INC. Access to the source code
-* contained herein is hereby forbidden to anyone except current SIBEL INC
-* employees, managers or contractors who have executed Confidentiality and
-* Non-disclosure agreements explicitly covering such access.
-* The copyright notice above does not evidence any actual or intended
-* publication or disclosure of this source code, which includes information that
-* is confidential and/or proprietary, and is a trade secret of SIBEL INC.
-*
-* ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC
-* DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN
-* CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
-* LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE
-* CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
-* REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
-* SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
-*
-*/
 
 // Zephyr dependent files
 #include <stdio.h>
